ede6f19ce5554c83230be94f52be312ea2d1a0d2
Sebastián Ponce <sebastian.ponce2511@gmail.com>
2023-02-15 14:20:49 -0300

    primer commit

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+import NewCustomer from "./pages/NewCustomer";
+import Index, { loader as customersLoader } from "./pages/Index";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Index />,
+        loader: customersLoader,
+      },
+      {
+        path: "/clientes/nuevo",
+        element: <NewCustomer />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/NewCustomer.jsx
@@ -0,0 +1,5 @@
+const NewCustomer = () => {
+  return <div>New Customer</div>;
+};
+
+export default NewCustomer;
